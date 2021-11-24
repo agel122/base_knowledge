22e7d8d9e4b81168868a6e423498dfdcbc9ab8f8
agel122 <agel122@yandex.ru>
2021-11-24 22:03:20 +0300

    decorator added with timer

--- base_knowledge/simple_decorator.py
+++ base_knowledge/simple_decorator.py
@@ -1,7 +1,13 @@
+import time
+
+
 def my_decorator(func):
     def wrapper(*args, **kwargs):
         print('yep')
-        return func(*args, **kwargs)
+        start_time = time.time()
+        res = func(*args, **kwargs)
+        end_time = time.time()
+        return (end_time - start_time), res, func.__name__
     return wrapper
 
 
@@ -10,4 +16,5 @@ def my_sum(x, y):
     return x+y
 
 
-my_sum(1, 2)
+print(my_sum(1, 2))
+
