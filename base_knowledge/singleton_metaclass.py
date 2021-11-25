101e1331ca5dc7c73ee3a509daac27b3c1ddc5a0
agel122 <agel122@yandex.ru>
2021-11-25 20:05:39 +0300

    added singleton_metaclass

---
+++ base_knowledge/singleton_metaclass.py
@@ -0,0 +1,17 @@
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class MyClass(metaclass=Singleton):
+    pass
+
+
+sample1 = MyClass()
+sample2 = MyClass()
+print(sample1)
+print(sample2)
